6bfd7d27a8f88776835eaaa72cc8bce6e46bcf3c
GideonCove <gideoncove@gmail.com>
2024-05-07 10:00:40 -0400

    dogbreed api added

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const myFunction = () => {
-    // console.log('First')
-    // console.log('Second')
-    // console.log('Third')
-    console.log('first')
-    axios.get('https://dog.ceo./api/breeds/list/all').then(function () {
-        console.log('second')
-    })
-    console.log('third')
+// const myFunction = () => {
+//     // console.log('First')
+//     // console.log('Second')
+//     // console.log('Third')
+//     console.log('first')
+//     axios.get('https://dog.ceo./api/breeds/list/all').then(function () {
+//         console.log('second')
+//     })
+//     console.log('third')
+// }
+
+const getBreeds = async () => {
+    const dogBreeds = await axios.get('https://dog.ceo/api/breeds/list/all')
+    console.log(dogBreeds.data.message)
 }
+
+getBreeds()
