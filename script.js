d7da39bb5d23efcbbf7a7b52aa38fb613c1669bb
GideonCove <gideoncove@gmail.com>
2024-05-07 09:46:01 -0400

    Added asynch

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-console.log('yo')
-console.log('testing!!!!!')
+const myFunction = () => {
+    // console.log('First')
+    // console.log('Second')
+    // console.log('Third')
+    console.log('first')
+    axios.get('https://dog.ceo./api/breeds/list/all').then(function () {
+        console.log('second')
+    })
+    console.log('third')
+}
